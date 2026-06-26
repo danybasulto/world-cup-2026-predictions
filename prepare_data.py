4ecfc0082e81adc2164376489e0d1acd33983690
danybt <danybt.dev@gmail.com>
2026-06-25 21:08:42 -0600

    feat(etl): actualizar los ranking

--- prepare_data.py
+++ prepare_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 df = pd.read_csv("./csv/historic-matches.csv")
 
+# Diccionario para guardar las actualizaciones
 teams_dict = {}
 
 # Recorrer cada partido
@@ -12,8 +13,18 @@ for idx, row in df.iterrows():
   team_b = row["equipo_b"]
 
   if pd.isna(row["elo_a"]):
-    # Si la fila esta vacia
-    pass
+    df.at[idx, "ranking_a"] = teams_dict[team_a]["ranking"]
+    df.at[idx, "elo_a"] = teams_dict[team_a]["elo"]
+    df.at[idx, "ranking_b"] = teams_dict[team_b]["ranking"]
+    df.at[idx, "elo_b"] = teams_dict[team_b]["elo"]
   else:
-    # Si la fila ya tiene datos
-    pass
+    teams_dict[team_a] = {
+      "ranking": row["ranking_a"],
+      "elo": row["elo_a"],
+      "last_matches": deque(maxlen=5)
+    }
+    teams_dict[team_b] = {
+      "ranking": row["ranking_b"],
+      "elo": row["elo_b"],
+      "last_matches": deque(maxlen=5)
+    }
